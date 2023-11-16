f315ffd4e6410cd9e76834508a0d53189c67fa5e
Mstgr <msteige@clemson.edu>
2023-11-16 17:27:17 -0500

    the joint works now

--- LabExamTwo_QuestionThree/src/QuestionThree/MyOnlyObject.java
+++ LabExamTwo_QuestionThree/src/QuestionThree/MyOnlyObject.java
@@ -8,7 +8,9 @@ public class MyOnlyObject
     //TODO: Create the constructor for MyOnlyObject. It doesn't need to do anything; there's no default value for amount.
 
     //put constructor here
-
+    private MyOnlyObject(){
+        // doesn't do anything because there is no default value for amount
+    }
     /*TODO:
      * Create and implement the static function called "getInstance()" used for returning the instance of MyOnlyObject.
      * In the "Lazy initialization" implementation of the Singleton Pattern, getInstance is where the singleton object
@@ -16,15 +18,25 @@ public class MyOnlyObject
      */
 
     //put getInstance here
+    public static MyOnlyObject getInstance(){
+        if (instance == null)
+            instance = new MyOnlyObject();
+        return instance;
+    }
 
     //TODO: Create and implement a function called getAmount(). It's a typically getter for the amount field.
     // There is no need to check that amount isn't null
 
     //put getAmount here
+    public Integer getAmount(){
+        return amount;
+    }
 
     //TODO: Create and implement a function called setAmount(). It's a typically setter for the amount field.
     // No input-validation needed.
 
     //put setAmount here
-
+    public void setAmount(Integer amount){
+        this.amount = amount;
+    }
 }
